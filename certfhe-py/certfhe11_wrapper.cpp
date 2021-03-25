a25c090f395b56ef6c107c3e1f88b3b688b52e8f
George Gugulea <gugulea@gmail.com>
2021-03-25 10:42:29 +0200

    added all exported classes, with all functions except operators

--- certfhe-py/certfhe11_wrapper.cpp
+++ certfhe-py/certfhe11_wrapper.cpp
@@ -1,8 +1,6 @@
 // pybind11_wrapper.cpp
 #include <pybind11/pybind11.h>
-#include <Context.h>
-
-#include <pybind11/pybind11.h>
+#include "certFHE.h"
 
 namespace py = pybind11;
 
@@ -12,10 +10,54 @@ PYBIND11_MODULE(certfhepy, m)
 {
     py::class_<Context>(m, "Context")
         .def(py::init<const uint64_t, const uint64_t>())
+        .def(py::init<Context&>())
         .def("getN", &Context::getN)
         .def("getD", &Context::getD)
         .def("getS", &Context::getS)
         .def("getDefaultN", &Context::getDefaultN)
         .def("setN", &Context::setN)
         .def("setD", &Context::setD);
+
+    py::class_<Permutation>(m, "Permutation")
+        .def(py::init<>())
+        .def(py::init<const uint64_t *, const uint64_t>())
+        .def(py::init<Context &>())
+        .def(py::init<const uint64_t>())
+        .def(py::init<const Permutation &>())
+        .def("getLength", &Permutation::getLength)
+        .def("getPermutation", &Permutation::getPermutation)
+        .def("getInverse", &Permutation::getInverse);
+
+    py::class_<Ciphertext>(m, "Ciphertext")
+        .def(py::init<>())
+        .def(py::init<const uint64_t *, const uint64_t *, const uint64_t, const Context &>())
+        .def(py::init<Ciphertext&>())
+        .def("setValues", &Ciphertext::setValues)
+        .def("setBitlen", &Ciphertext::setBitlen)
+        .def("setContext", &Ciphertext::setContext)
+        .def("getLen", &Ciphertext::getLen)
+        .def("getContext", &Ciphertext::getContext)
+        .def("getValues", &Ciphertext::getValues)
+        .def("getBitlen", &Ciphertext::getBitlen)
+        .def("applyPermutation_inplace", &Ciphertext::applyPermutation_inplace)
+        .def("applyPermutation", &Ciphertext::applyPermutation)
+        .def("size", &Ciphertext::size);
+
+    py::class_<Plaintext>(m, "Plaintext")
+        .def(py::init<>())
+        .def(py::init<const int>())
+        .def("getValue", &Plaintext::getValue)
+        .def("setValue", &Plaintext::setValue);
+
+    py::class_<SecretKey>(m, "SecretKey")
+        .def(py::init<const Context&>())
+        .def(py::init<const SecretKey&>())
+        .def("encrypt", static_cast<Ciphertext (SecretKey::*)(Plaintext &)>(&SecretKey::encrypt))
+        .def("decrypt", static_cast<Plaintext (SecretKey::*)(Ciphertext &)>(&SecretKey::decrypt))
+        .def("applyPermutation_inplace", &SecretKey::applyPermutation_inplace)
+        .def("applyPermutation", &SecretKey::applyPermutation)
+        .def("getLength", &SecretKey::getLength)
+        .def("getKey", &SecretKey::getKey)
+        .def("setKey", &SecretKey::setKey)
+        .def("size", &SecretKey::size);
 }
